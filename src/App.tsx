11f518dec0bea73aca3bb3ff666cc1b1dc52b1f8
Ryan Graciano <ryan@ryangraciano.com>
2019-02-27 13:38:01 -0800

    Figuring out how to use React

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi! This is a test. Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export class StringMap { [s: string]: string; }
+
+class Card extends Component<{cardName: string, cardId: string}> {
+    render() {
+        return (
+            <span className="Card" key={this.props.cardId}>[{this.props.cardName}]&nbsp;&nbsp;</span>
+        );
+    }
 }
 
-export default App;
+class Patroller extends Component<{patrollerSlot:string, cardName: string, cardId: string}> {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.patrollerSlot}: <Card cardName={this.props.cardName} cardId={this.props.cardId} /></h3>
+            </div>
+        )
+    }
+}
+
+export class CardList extends Component<{listName: string, cardObjects: StringMap[]}> {
+
+    cards(cardObjects: StringMap[]) {
+        return cardObjects.map(cardObj => <Card cardId={cardObj.cardId} cardName={cardObj.cardName}></Card>);
+    }
+
+    render() {
+        return (
+           <div>{this.props.listName}: { this.cards(this.props.cardObjects) }</div>
+        );
+    }
+}
+
+export class CodexGame extends Component {
+    render() {
+        return(
+            <div>
+                <div>
+                    <h1>Opponent Board</h1>
+                    <h2>Patrollers</h2>
+                    <h3>Squad Leader: </h3>
+                </div>
+            </div>
+        )
+    };
+}
+
+export default CardList;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { CardList, CodexGame } from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let cardObjects = [ { cardId: 'first', cardName: 'imaninja' }, { cardId: 'second', cardName: 'numbatwo' }];
+
+//ReactDOM.render(<Hand cardObjects={cardObjs} />, document.getElementById('root'));
+
+//ReactDOM.render(<CardList listName="Hand" cardObjects={cardObjects} />, document.getElementById('root'));
+
+ReactDOM.render(<CodexGame />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
